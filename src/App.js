05eb503d79c1ff0f1ab127ecdc878f659b267f39
AlexCiobanu47 <alexciobanu72@yahoo.com>
2023-04-26 19:27:28 +0300

    changed code for gh pages deployment

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 //pages and components
-import Home from "./pages/home";
 import Navbar from "./components/Navbar";
+import Home from "./pages/home";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-      <Navbar></Navbar>
+      <HashRouter>
+        <Navbar></Navbar>
         <div className="pages">
           <Routes>
             <Route path="/" element={<Home />}></Route>
           </Routes>
         </div>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
